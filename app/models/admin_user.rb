4ec57367a6ff7b82cf46ca9978c28616a2f8d59b
gongchen161 <gong.chen161@gmail.com>
2018-08-23 14:21:37 -0400

    data validations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,18 @@ class AdminUser < ApplicationRecord
 
 	has_many :section_edits
 	has_many :sections, :through => :section_edits
+
+	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+
+	validates :first_name, :presence => true,
+							:length => { :maximum => 25 }
+    validates :last_name, :presence => true,
+                        	:length => { :maximum => 50 }
+  	validates :username, :length => { :within => 8..25 },
+                       		:uniqueness => true
+  	validates :email, :presence => true,
+                    	:length => { :maximum => 100 },
+                    	:format => EMAIL_REGEX,
+                    :key => "value", confirmation => true
+
 end
